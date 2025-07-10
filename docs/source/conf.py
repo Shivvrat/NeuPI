e480bbf652665eb232c725557904669ce9c88813
Shivvrat Arya <shivvrat.arya@utdallas.edu>
2025-07-09 23:05:13 -0500

    add cython files to autodoc_mock_imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ extensions = [
 # MOCK IMPORTS: This is the crucial addition.
 # It tells Sphinx to ignore these modules, which might fail to import in the
 # Read the Docs build environment, allowing the documentation to build correctly.
-autodoc_mock_imports = ["torch", "numpy", "neupi.training.pm_ssl.io.uai_reader_cython"]
+autodoc_mock_imports = ["torch", "numpy", "neupi.training.pm_ssl.io.uai_reader_cython", "neupi.discretize.kn.cython_kn.kn_binary_vectors"]
 
 templates_path = ["_templates"]
 exclude_patterns = []
